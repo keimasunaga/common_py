7d7e6ef3292dfedcdc51ac928b9af63e28e53320
Kei Masunaga <keimasunaga@gmail.com>
2020-11-09 18:23:59 -0700

    Add isnotebook function
    This judges if the code is run in jupyter notebook or not.

--- tools.py
+++ tools.py
@@ -255,6 +255,19 @@ class RunTime:
         print ("Runtime:{0}".format(runtime) + "[sec]")
         print ("Runtime:{0}".format(runtime/60) + "[min]")
 
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
+
 ############################
 #### plot related tools ####
 ############################
